e1cc15d1bbf39779bb55aba4e0f47c8e5099a32f
railken <railken@protonmail.ch>
2018-06-08 05:10:30 +0200

    update Router::group()

--- src/Api/Support/Router.php
+++ src/Api/Support/Router.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Config;
 
 class Router
 {
-    public static function group(\Closure $closure)
+    public static function group(array $config, \Closure $closure)
     {
-        return Route::group(Config::get('ore.api.router'), function ($router) use ($closure) {
-            return $closure($router);
+        return Route::group(Config::get('ore.api.router'), function ($router) use ($config, $closure) {
+            return Route::group($config, $closure);
         });
     }
 }
