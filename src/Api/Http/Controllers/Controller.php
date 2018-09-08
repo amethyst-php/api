316e3bd77263584c409bba4cbe80b599261d5e3e
railken <railken@protonmail.ch>
2018-09-08 19:04:27 +0200

    update responses

--- src/Api/Http/Controllers/Controller.php
+++ src/Api/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
@@ -23,9 +24,39 @@ class Controller extends BaseController
      * @return \Symfony\Component\HttpFoundation\Response
      * @static
      */
-    public function response($data = [], $status = 200, $headers = [])
+    public function response($data = [], $status = Response::HTTP_OK, $headers = [])
     {
         return new JsonResponse($data, $status, $headers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    }
+
+    /**
+     * Return a new JSON response from the application.
+     *
+     * @param string|array $data
+     * @param int          $status
+     * @param array        $headers
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @static
+     */
+    public function error($data = [], $status = Response::HTTP_BAD_REQUEST, $headers = [])
+    {
+        return $this->response($data, $status, $headers);
+    }
+
+    /**
+     * Return a new JSON response from the application.
+     *
+     * @param string|array $data
+     * @param int          $status
+     * @param array        $headers
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @static
+     */
+    public function success($data = [], $status = Response::HTTP_OK, $headers = [])
+    {
+        return $this->response($data, $status, $headers);
     }
 
     /**
